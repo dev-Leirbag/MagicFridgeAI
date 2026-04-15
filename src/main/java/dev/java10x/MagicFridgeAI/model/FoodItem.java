aa628ded0c0d79e4930a6769b4f0b5f44621c83a
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-14 22:38:55 -0300

    feat: Cria o package model e as classes FoodItem junto com o FoodItemEnum

---
+++ src/main/java/dev/java10x/MagicFridgeAI/model/FoodItem.java
@@ -0,0 +1,28 @@
+package dev.java10x.MagicFridgeAI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "food_item")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FoodItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private FoodItemEnum categoria;
+
+    private Integer quantidade;
+
+    private LocalDate validade;
+}
---
+++ src/main/java/dev/java10x/MagicFridgeAI/model/FoodItemEnum.java
@@ -0,0 +1,7 @@
+package dev.java10x.MagicFridgeAI.model;
+
+public enum FoodItemEnum {
+    VEGETAIS,
+    CARNE,
+    LATICINIOS
+}

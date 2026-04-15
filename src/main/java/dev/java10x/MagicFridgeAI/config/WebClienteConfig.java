e11b72779439320227722196fda309b094afec53
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-14 22:45:59 -0300

    feat: Adiciona a configuracao para utilizar o WebClient

---
+++ src/main/java/dev/java10x/MagicFridgeAI/config/WebClienteConfig.java
@@ -0,0 +1,19 @@
+package dev.java10x.MagicFridgeAI.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClienteConfig {
+
+    @Value("${chatgpt.url}")
+    private String chatGptUrl;
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(chatGptUrl).build();
+    }
+
+}

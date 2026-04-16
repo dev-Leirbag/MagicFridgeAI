8e9e87fe45c1d47c0f9f96a65a9da1689dd1fd02
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-15 23:39:29 -0300

    feat: Adiciona na classe WebClienteConfig um @Bean para um melhor funcionamento do build da URI

--- src/main/java/dev/java10x/MagicFridgeAI/config/WebClienteConfig.java
+++ src/main/java/dev/java10x/MagicFridgeAI/config/WebClienteConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @Configuration
 public class WebClienteConfig {
@@ -12,10 +13,13 @@ public class WebClienteConfig {
     private String chatGptUrl;
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .baseUrl(chatGptUrl)
-                .build();
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(chatGptUrl).build();
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
 }
